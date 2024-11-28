693248da93bdedce32e0e7929d980b0b5a8fae4f
Gabriel da Silva <00001094129549sp@al.educacao.sp.gov.br>
2024-11-28 09:51:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemátia',
+    'O que é pi (π)?',
+    'Representa o valor da razão entre a circunferência de qualquer círculo e seu diâmetro.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro homem a pisar na lua?',
+    'O piloto militar, piloto de testes e engenheiro aeroespacial, Neil Armstrong, se tornou o primeiro ser humano a pisar na Lua.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da suíça?',
+    'A Suíça tem como capital a cidade de Berna.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é uma charge?',
+    'Charge é um gênero textual jornalístico que tem como intenção principal fazer uma crítica sobre algum tema usando o humor.'
+)
